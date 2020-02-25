0ea32ccad189043ef19460ba5ac12cc9029a8bff
Moonshot <moonshot11@users.noreply.github.com>
2020-02-24 21:11:13 -0800

    Create monster list based on scaled XP tables

--- gen.py
+++ gen.py
@@ -15,7 +15,9 @@ def setup_args():
     """Setup arguments"""
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("difficulty", help="Sum of players' levels", type=float)
+    parser.add_argument("levels",
+            help="Space-separated string of player levels")
+    parser.add_argument("difficulty", choices=["easy", "med", "hard", "deadly"])
     parser.add_argument("--max-per-group", "-m", default=4, type=int,
             help="Max amt per group")
     parser.add_argument("--orcs", help="Require orcs", action="store_true")
@@ -24,12 +26,22 @@ def setup_args():
     parser.add_argument("--input-file", default="srd5e_monsters.txt",
             help="The data file with monster information")
     parser.add_argument("--min-factor", "-mn", default=8, type=float,
-            help="Min div CR factor (default: 8)")
+            help="Min mult factor (default: 0.25)")
     parser.add_argument("--max-factor", "-mx", default=1, type=float,
-            help="Max div CR factor (default: 1)")
+            help="Max mult factor (default: 1)")
 
     args = parser.parse_args()
     return args
+
+def multiply(xp, amt):
+    """Apply multiplier to xp based on number of enemies"""
+    if amt == 0: return 0
+    if amt == 1: return xp
+    if amt == 2: return 1.5 * xp
+    if amt in range(3, 7): return 2 * xp
+    if amt in range(7, 11): return 2.5 * xp
+    if amt in range(11, 15): return 3 * xp
+    return 4 * xp
 
 def pick_random(monsters, floor, ceil):
     """Get candidates as tuples"""
@@ -38,6 +50,25 @@ def pick_random(monsters, floor, ceil):
         if floor <= mon.rating <= ceil:
             candidates.append(mon)
     return random.choice(candidates)
+
+def calc_target_xp(levels, difficulty):
+    """Get target XP from table"""
+    diffs = ["easy", "med", "hard", "deadly"]
+
+    with open("thresholds.csv", "r") as fin:
+        lines = [line.strip() for line in fin.readlines()
+                 if line.strip() and re.match(r"[0-9,]+$", line.strip())]
+
+    targets = {}
+    for line in lines:
+        line = line.split(",")
+        targets[line[0]] = line[1:]
+
+    total = 0
+    for lvl in levels.split():
+        total += int(targets[lvl][diffs.index(difficulty)])
+
+    return total
 
 def find_monster(monsters, name):
     """Return first Monster item with name"""
@@ -63,7 +94,7 @@ def init_data(filename):
         match_obj = re.search(r"^(?:0\.)?(\d+)\s+\((\d+)\s*XP\s*\)$",
                 line.replace(",", ""))
         if match_obj:
-            rating = 4.0 * float(match_obj.group(1))
+            rating = float(match_obj.group(1))
             xp = int(match_obj.group(2))
             continue
         if rating is None or xp is None:
@@ -79,37 +110,48 @@ if __name__ == "__main__":
     args = setup_args()
     monsters = init_data(args.input_file)
     orc = find_monster(monsters, "Orc")
-    difficulty = args.difficulty
+    target_xp_ceil = calc_target_xp(args.levels, args.difficulty)
+    target_xp_flr = target_xp_ceil * 0.9
+
     result = {}
+    xp_total = 0
+    monster_count = 0
 
-    while difficulty > 0:
-        # If (somehow) there are no candidates remaining, bail
-        if not monsters:
-            break
-
+    while not (target_xp_flr < multiply(xp_total, monster_count) and
+               multiply(xp_total, monster_count) <= target_xp_ceil):
         # Add Orcs if specifically required
         if not result and args.orcs:
             winner = orc
             amt = random.randint(2, 8)
-            if difficulty < winner.rating:
+            if target_xp_ceil < winner.xp:
                 print("WARNING: Orcs too difficult for this group")
         else:
-            winner = pick_random(monsters, difficulty / args.min_factor,
-                    difficulty / args.max_factor)
+            # Populate candidates and choose who's next
+            candidates = []
+            for mon in monsters:
+                if multiply(xp_total + mon.xp, monster_count + 1) < target_xp_ceil:
+                    candidates.append(mon)
+            # If no available creatures, bail
+            if not candidates:
+                break
+            winner = random.choice(candidates)
             amt = random.randint(1, args.max_per_group)
 
         # Remove chosen monster from further candidacy
         monsters.remove(winner)
 
         while amt > 0:
-            difficulty -= winner.rating
+            xp_total += winner.xp
             result[winner] = result.get(winner, 0) + 1
             amt -= 1
-            if difficulty - winner.rating < 0:
+            monster_count += 1
+            if multiply(xp_total + winner.xp, monster_count + 1) > target_xp_ceil:
                 break
  
     print()
     total = 0
+    count = 0
     for monster, amt in result.items():
         print("{} x{}".format(monster.name, amt))
-        total += monster.rating * amt
+        total += monster.xp * amt
+        count += amt
